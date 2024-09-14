df52290a2cb424515440a0325f2f67001bcc2f49
KRS_GOLF <korawit7680@gmail.com>
2024-09-14 16:52:26 +0700

    Update todo.js

--- cell03/ex04/ex03bis/todo.js
+++ cell03/ex04/ex03bis/todo.js
@@ -1,40 +1,41 @@
 $(document).ready(() => {
-  let ft_list = $("#ft_list");
-
-  function loadTasks() {
-      const cookies = document.cookie.split("; ");
-      cookies.forEach((cookie) => {
-          let [name, value] = cookie.split("=");
-          if (value) {
-              let list = $("<div></div>").text(decodeURIComponent(value));
-              list.click(() => {
-                  const check = confirm("Do you want to delete");
-                  if (check) {
-                      list.remove();
-                      document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`;
-                  }
-              });
-              ft_list.prepend(list);
-          }
-      });
-  }
-
-  loadTasks();
-
-  $("#create").click(() => {
-      let txt = prompt("Please Enter TODO LIST :");
-      if (txt) {
-          const name = new Date().getTime();
-          document.cookie = `${name}=${encodeURIComponent(txt)}; path=/`;
-          let list = $("<div></div>").text(txt);
-          list.click(() => {
-              const check = confirm("Do you want to delete");
-              if (check) {
-                  list.remove();
-                  document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`;
-              }
-          });
-          ft_list.prepend(list);
-      }
+    let ft_list = $("#ft_list");
+  
+    function loadTasks() {
+        const cookies = document.cookie.split("; ");
+        cookies.forEach((cookie) => {
+            let [name, value] = cookie.split("=");
+            if (value) {
+                let list = $("<div></div>").text(decodeURIComponent(value)).addClass("todo");
+                list.click(() => {
+                    const check = confirm("Do you want to delete");
+                    if (check) {
+                        list.remove();
+                        document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`;
+                    }
+                });
+                ft_list.prepend(list);
+            }
+        });
+    }
+  
+    loadTasks();
+  
+    $("#create").click(() => {
+        let txt = prompt("Please Enter TODO LIST :");
+        if (txt) {
+            const name = new Date().getTime();
+            document.cookie = `${name}=${encodeURIComponent(txt)}; path=/`;
+            let list = $("<div></div>").text(txt).addClass("todo");
+            list.click(() => {
+                const check = confirm("Do you want to delete");
+                if (check) {
+                    list.remove();
+                    document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`;
+                }
+            });
+            ft_list.prepend(list);
+        }
+    });
   });
-});
+  
